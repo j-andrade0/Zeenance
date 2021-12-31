71fb419ee7746d123d6ee14da879f816fc633190
Jose Antonio <andradejoseantonio0@gmail.com>
2021-12-30 22:41:40 -0300

    criaçao do banco antes do primeiro request e modo de debug ativado

--- backend/app.py
+++ backend/app.py
@@ -12,9 +12,21 @@ config = json.load(config_file)
 app = Flask(__name__)
 CORS(app)
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = config['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config['SQLALCHEMY_TRACK_MODIFICATIONS']
 app.config['JWT_SECRET_KEY'] = config['JWT_SECRET_KEY']
 app.config['JWT_BLACKLIST_ENABLED'] = config['JWT_BLACKLIST_ENABLED']
 
+
 jwt = JWTManager(app)
+
+
+@app.before_first_request
+def create_database():
+    db.create_all()
+
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(debug=True)
