07601fb55e171af292de49a68625fb93dadc553f
Jose Antonio <andradejoseantonio0@gmail.com>
2022-01-01 22:20:56 -0300

    criaçao de usuario_model

---
+++ backend/model/usuario_model.py
@@ -0,0 +1,79 @@
+from util.db import db
+
+class UsuarioModel(db.Model):
+    __tablename__ = 'tb_usuario'
+
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(100), unique = True)
+    senha = db.Column(db.String(32))
+
+
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+# API Methods
+
+    def get_all(self):
+        return UsuarioModel.find_all().json()
+
+
+    def get_by_id(self, id):
+        return UsuarioModel.find_by_id(id).json()
+
+
+    def post(self):
+        UsuarioModel.add()
+
+
+    def put(self, id):
+        UsuarioModel.update(id)
+
+
+    def delete(self):
+        UsuarioModel.find_by_id(id)
+        UsuarioModel.delete()
+
+
+# Class methods
+
+    @classmethod
+    def find_all(cls):
+        for found in cls.query.all():
+            return found
+    
+
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id)
+    
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    def remove(self):
+        db.session.delete(self)
+        db.session.commit
+
+
+    def update(self, **dados):
+        UsuarioModel.find_by_id(id).update(dict(**dados))
+        db.session.commit()
+
+
+    def json(self):
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'email': self.email,
+            'senha': self.senha
+        }
+
+
+    def __repr__(self):
+        return '<Usuario %r>' % self.id
