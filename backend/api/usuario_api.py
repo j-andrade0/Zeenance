f6ba6a8cf32b86295aede6be808da972b48bae40
Jose Antonio <andradejoseantonio0@gmail.com>
2022-01-02 00:04:17 -0300

    criacao da api

---
+++ backend/api/usuario_api.py
@@ -0,0 +1,25 @@
+from flask import Blueprint
+import controller.usuario_controller as controller
+
+
+usuario_api = Blueprint('usuario_api', __name__)
+
+
+@usuario_api.route('/usuario', methods=['GET'])
+def get_all():
+    return controller.get_all()
+
+
+@usuario_api.route('/usuario/<id>', methods=['GET'])
+def get_by_id(id):
+    return controller.get_by_id(id)
+
+
+@usuario_api.route('/usuario', methods=['POST'])
+def post():
+    return controller.post()
+
+
+@usuario_api.route('/usuario/<id>', methods=['PUT'])
+def put(id):
+    return controller.put(id)
