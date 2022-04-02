6704de775ea2503092a56876a9d5736b11ba8e87
Jose Antonio <andradejoseantonio0@gmail.com>
2022-04-02 11:06:23 -0300

    atualizacao do jwtrequired para jwtrequired() para atender ao padrao da atualizacao da lib

--- backend/controller/usuario_controller.py
+++ backend/controller/usuario_controller.py
@@ -3,27 +3,27 @@ import service.usuario_service as service
 from flask_jwt_extended import jwt_required
 
 
-@jwt_required
+@jwt_required()
 def get_all():
     return jsonify(service.get_all()), 200
 
 
-# @jwt_required
+@jwt_required()
 def get_by_id(id):
     return jsonify(service.get_by_id(id)), 200
 
 
-@jwt_required
+@jwt_required()
 def post():
     return service.post(), 201
 
 
-@jwt_required
+@jwt_required()
 def put(id):
     return service.put(id), 200
 
 
-@jwt_required
+@jwt_required()
 def delete(id):
     return service.delete(id), 200
 
