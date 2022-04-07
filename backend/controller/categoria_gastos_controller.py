4cc497d40c6b485e1e97c76bacdd5ea2434cc863
Jose Antonio <andradejoseantonio0@gmail.com>
2022-04-07 16:41:47 -0300

    necessidade de autenticacao para rotas de categoria_gastos

--- backend/controller/categoria_gastos_controller.py
+++ backend/controller/categoria_gastos_controller.py
@@ -1,22 +1,28 @@
 from flask import jsonify
 import service.categoria_gastos_service as service
+from flask_jwt_extended import jwt_required
 
 
+@jwt_required()
 def get_all():
     return jsonify(service.get_all()), 200
 
 
+@jwt_required()
 def get_by_id(id):
     return jsonify(service.get_by_id(id)), 200
 
 
+@jwt_required()
 def post():
     return service.post(), 201
 
 
+@jwt_required()
 def put(id):
     return service.put(id), 200
 
 
+@jwt_required()
 def delete(id):
     return service.delete(id), 200
